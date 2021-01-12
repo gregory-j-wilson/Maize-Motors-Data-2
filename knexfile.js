445a977b67e1deee00bab5c444aa9eb306505fb4
Greg Wilson <gjwilson7390@gmail.com>
2021-01-12 01:57:17 -0600

    ssl on

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,10 @@
 // Update with your config settings.
 
-var dotenv = require('dotenv')
-dotenv.config({ path: './.env'})
+// var dotenv = require('dotenv')
+// dotenv.config({ path: './.env'})
+
+// var pg = require('pg');
+// pg.defaults.ssl = true;
 
 module.exports = {
 
@@ -14,7 +17,8 @@ module.exports = {
     },
     seeds: {
       directory: './database/seeds'
-    }
+    },
+    ssl: true
   },
 
   testing: {
@@ -41,13 +45,21 @@ module.exports = {
   production: {
     client: 'pg',
     useNullAsDefault: true,
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: "ec2-52-44-46-66.compute-1.amazonaws.com",
+      port: 5432,
+      user: "svyllxwlkngwhk",
+      password: "48064fcca22c1a5ccb26ce6430351e5618a12f1edb49663eedde5807ce83be1e",
+      database: "d245s3eoecht9v",
+      ssl: true,
+     },
     migrations: {
       directory: './database/migrations' 
     },
     seeds: {
       directory: './database/seeds'
-    }
+    },
+    ssl: true
   }
 
 };
