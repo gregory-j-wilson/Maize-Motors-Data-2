56fdc3f648d4fef91ef7142c62f9563a91da189f
Greg Wilson <gjwilson7390@gmail.com>
2021-02-22 13:42:26 -0600

    exports down for longer commentary

--- database/migrations/20210222114611_longer_text_chapter_commentary.js
+++ database/migrations/20210222114611_longer_text_chapter_commentary.js
@@ -9,8 +9,8 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
 
-    return knex.schema.alterTable('commentary', function(t) {
-        t.string('commentary').notNullable().alter();
-      });
+    return knex.schema.table('commentary', function(t) {
+        t.dropColumn('commentary') 
+    })
 
 };
